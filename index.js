b8e0423e92167fe134e4bd5d8389ece280137993
nigam <nigamnayak9@gmail.com>
2020-11-30 20:57:03 +0530

    ejs demo added for title and a simple for loop

--- index.js
+++ index.js
@@ -10,15 +10,23 @@ app.set('views',path.join(__dirname,'views'));//Setting path for view engine
 
 //////------Controller part Starts Here ------//////
  app.get('/',function(req,res){
-    return res.render('home');//we must return from here by rendering html file
-    //other wise it will keep searching ...
+    return res.render('home',{title: "Nigam"});/*we must return from here by rendering html file
+    other wise it will keep searching ...
+    and we must set a java script object along with 
+    rendered view file(html file) to make web site dynamic 
+    java script object will be sent to browser .
+    In the dynamic ejs(html) file when key of js will be there ,
+    upon rendering value of corresponding key will be replaced .
+    */
+
+
     //res.send("YEY !");
     //console.log(req.url);
 });
 
 //////------Controller part Ends  Here ------//////
 
- app.listen(port,function(err){//make the server listen 
+ app.listen(port,function(err){   //make the server listen 
      if(err){
          console.log("error running on server",err);
      }
