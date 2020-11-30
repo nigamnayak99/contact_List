41e08762f4ede2e3c74a9ac835233a6713160d0f
nigam <nigamnayak9@gmail.com>
2020-11-30 19:41:25 +0530

    Ejs installed , ejs template_engine added , path module required ,set view engine as ejs , set views(html) paths,rendered and returned the home.ejs(html)

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
  const express = require('express');//importing the dependency library
+ const path = require('path');
  const port = 8000;// defining port number
 
  const app = express();// Creating object of express
+app.set('view engine','ejs');//making node know that view engine is ejs
+app.set('views',path.join(__dirname,'views'));//Setting path for view engine
 
+
+
+//////------Controller part Starts Here ------//////
  app.get('/',function(req,res){
-    res.send("YEY !");
-     console.log(req.url);
+    return res.render('home');//we must return from here by rendering html file
+    //other wise it will keep searching ...
+    //res.send("YEY !");
+    //console.log(req.url);
 });
 
-
+//////------Controller part Ends  Here ------//////
 
  app.listen(port,function(err){//make the server listen 
      if(err){
