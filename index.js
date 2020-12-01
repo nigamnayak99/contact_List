d868e0b90ffb92640c69163aeb75877fd1c6398a
nigam <nigamnayak9@gmail.com>
2020-12-01 21:23:24 +0530

    1.created a practice view for testing,2.created a js variable that holds data to be send to view send data as js object to target view with help of controller print incoming data from server to view in view page added a form to take data from user and add it parser hasnt not been added yet , which needs to read data that is availaible with response upon submitting it will again redirect to main page controller

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
- const express = require('express');//importing the dependency library
+ const { response } = require('express');
+const express = require('express');//importing the dependency library
  const path = require('path');
  const port = 8000;// defining port number
 
- const app = express();// Creating object of express
-app.set('view engine','ejs');//making node know that view engine is ejs
-app.set('views',path.join(__dirname,'views'));//Setting path for view engine
+ const app = express();  // Creating object of express
+app.set('view engine','ejs');  //making node know that view engine is ejs
+app.set('views',path.join(__dirname,'views'));  //Setting path for view engine
 
+var contactList = [
+    {
+        name : "Nigam ",
+        phone : "8658261067"
+
+    },
+    {
+        name : "Nigam 2 ",
+        phone : "8658261067 2"
+
+    },
+    {
+        name : "Nigam 3",
+        phone : "86582610673"
+
+    }
+
+]
+//this is variable that is send as an object to view
 
 
 //////------Controller part Starts Here ------//////
- app.get('/',function(req,res){
-    return res.render('home',{title: "Nigam"});/*we must return from here by rendering html file
+app.get('/',function(req,res){
+    return res.render('home',{
+                        title: "Nigam",//title is send here
+                        contact_list:contactList // contact is send here
+                    });
+    /*we must return from here by rendering html file
     other wise it will keep searching ...
     and we must set a java script object along with 
     rendered view file(html file) to make web site dynamic 
@@ -24,6 +48,24 @@ app.set('views',path.join(__dirname,'views'));//Setting path for view engine
     //console.log(req.url);
 });
 
+
+
+app.get('/practice',function(req,res){
+    return res.render('practice',
+    {
+        title:"Practice"
+    }
+    );
+});
+//above controller is for practice view
+
+
+
+
+app.post('/create-contact',function(req,res){
+    return res.redirect('/');
+
+});
 //////------Controller part Ends  Here ------//////
 
  app.listen(port,function(err){   //make the server listen 
